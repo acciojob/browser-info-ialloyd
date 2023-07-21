e44494e6362edb29b9e0a8c85174e64b63dcac1d
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-21 16:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+let userAgent = navigator.userAgent;
+let browserName, version;
+
+if (userAgent.indexOf("Chrome") > -1) {
+  browserName = "Chrome";
+  let start = userAgent.indexOf("Chrome/") + 7;
+  let end = userAgent.indexOf(" ", start);
+  version = userAgent.substring(start, end);
+}
+
+let message = `You are using ${browserName} version ${version}`;
+
+let browserInfoDiv = document.getElementById("browser-info");
+
+browserInfoDiv.textContent = message;
+
+
